5920fb98536f7fa8b94e857ea35395714e5c532f
afrolionx <mike@mikecorp.ca>
2018-07-18 04:08:25 -0400

    added a regex and replace on the logSection to remove console colouring artifacts

--- parcers/travis-logs.js
+++ parcers/travis-logs.js
@@ -3,9 +3,12 @@
 const START = 'travis_time:start:';
 const END = 'travis_time:end:';
 
+const cleaner = /(\?\[0K)|(\?\[\d+\;)|(\?\[\d+m)|(\?\d+m)|(travis_time\:start\:[0-9a-z]+)/gm;
+
 module.exports = (log) => {
   const startIndex = log.lastIndexOf(START);
   const endIndex = log.lastIndexOf(END);
-  const testLog = log.slice(startIndex, endIndex);
-  return testLog;
+  const logSection = log.slice(startIndex, endIndex);
+
+  return logSection.replace(cleaner, "");
 };
