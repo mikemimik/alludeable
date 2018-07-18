81a5396e54f639f8a22229d612f52b9e67333f62
afrolionx <mike@mikecorp.ca>
2018-07-18 05:08:48 -0400

    updated regex that cleans travis logs

--- parcers/travis-logs.js
+++ parcers/travis-logs.js
@@ -3,7 +3,7 @@
 const START = 'travis_time:start:';
 const END = 'travis_time:end:';
 
-const cleaner = /(\?\[0K)|(\?\[\d+\;)|(\?\[\d+m)|(\?\d+m)|(travis_time\:start\:[0-9a-z]+)/gm;
+const cleaner = /(\?\[\d+\S)|(\?\[\d+\;)|(\?\[\d+m)|(\?\d+m)|(\?\[\d\?)|(\?\[K)|(travis_time\:start\:[0-9a-z]+)/gm;
 
 module.exports = (log) => {
   const startIndex = log.lastIndexOf(START);
