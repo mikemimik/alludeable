334cad50f4b66ccf787732f77b2ccd86055bf7c5
afrolionx <mike@mikecorp.ca>
2018-07-20 01:58:50 -0400

    added comments to index file

--- index.js
+++ index.js
@@ -2,14 +2,30 @@
 
 const fs = require('fs');
 
+// INFO(mperrotte): read the listeners directory
 const files = fs.readdirSync(`${__dirname}/listeners`, 'utf8');
 
+/**
+ * INFO(mperrotte):
+ * Iterate over all the files from the listeners directory
+ * and build an array of functions.
+ */
 const listeners = files.reduce((acc, filename) => {
   const listener = require(`./listeners/${filename}`);
   return acc.concat([listener]);
 }, []);
 
+/**
+ * Main exported function for probot to consume
+ * @param {App} app The app passed in by probot
+ */
 module.exports = (app) => {
+
+  /**
+   * INFO(mperrotte):
+   * loop array of functions and pass app context into
+   * each of the listener functions.
+   */
   listeners.forEach((listener) => {
     listener(app);
   });
