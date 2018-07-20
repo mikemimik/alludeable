3285e20ceaea0f58f4d7a4b86c3a70c334c0fe6a
afrolionx <mike@mikecorp.ca>
2018-07-20 01:55:56 -0400

    added comments to the travis parcer file

--- parcers/travis.js
+++ parcers/travis.js
@@ -5,10 +5,20 @@ const END = 'travis_time:end:';
 
 const cleaner = /(\?\[\d+\S)|(\?\[\d+\;)|(\?\[\d+m)|(\?\d+m)|(\?\[\d\?)|(\?\[K)|(travis_time\:start\:[0-9a-z]+)/gm;
 
+/**
+ * Parses the incoming log string for the last section which
+ * in the context of Travic CI is the test output.
+ * @param {string} log The log string to parse
+ */
 module.exports = (log) => {
   const startIndex = log.lastIndexOf(START);
   const endIndex = log.lastIndexOf(END);
   const logSection = log.slice(startIndex, endIndex);
 
+  /**
+   * INFO(mperrotte):
+   * We're stripping the colour information out of the
+   * returned log section to make it more readable.
+   */
   return logSection.replace(cleaner, "");
 };
